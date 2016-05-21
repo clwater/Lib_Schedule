00fa928c74c8fe4b3d6f5e270d72dd4a8750e3fc
clwater <gengzhibo@gengzhibo.net>
2016-05-21 15:19:49 +0800

    20160521

--- app/src/main/java/com/clwater/lib_schedule/MainActivity.java
+++ app/src/main/java/com/clwater/lib_schedule/MainActivity.java
@@ -1,9 +1,9 @@
 package com.clwater.lib_schedule;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
